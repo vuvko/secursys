42a3cb0d086d43f77cee7d22ed8ff0a7cb432b74
Alexander Turenko <alextux@bk.ru>
2013-12-19 10:57:27 +0400

    Add forgotten.

---
+++ cryptofallback.h
@@ -0,0 +1,34 @@
+#ifndef CryptoFallback_H
+#define CryptoFallback_H
+
+void hash_func_256_fallback(const unsigned char *, unsigned long long, unsigned char *out)
+{
+    memset(out, 1, 32);
+}
+
+void hash_func_512_fallback(const unsigned char *, unsigned long long, unsigned char *out)
+{
+    memset(out, 1, 64);
+}
+
+int encrypt_func_fallback(unsigned char *f, unsigned long c, unsigned char *, unsigned char *out)
+{
+    memcpy(out, f, c);
+    if (c % 16)
+        memset(out + c, 0, c % 16);
+    return c;
+}
+
+int decrypt_func_fallback(unsigned char *f, unsigned long c, unsigned char *, unsigned char *out, unsigned long *)
+{
+    memcpy(out, f, c);
+    return c;
+}
+
+int gen_func_fallback(unsigned char *out, unsigned long c)
+{
+    memset(out, 1, c);
+    return c;
+}
+
+#endif // CryptoFallback_H
